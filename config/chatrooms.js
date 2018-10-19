262ca6b692756d25ac533dffe715904bb485523a
nileshgulia1 <nileshgulia@gmail.com>
2018-10-19 22:42:23 +0530

    add base files :fire:

---
+++ config/chatrooms.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    name: 'Room 1 in Costa brava',
+    image: 'chatrooms/costabrava.jpg'
+  },
+  {
+    name: 'Room 2 in Manresa HA ha',
+    image: 'chatrooms/Manresa.jpg'
+  },
+]
+
---
+++ config/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'User1',
+    lastName: 'Heyaa',
+    statusText: 'I am the leader!',
+    //image: 
+  },
+  {
+    name: 'User 2',
+    lastName: '',
+    statusText: 'Tokyo.'
+  },
+  {
+    name: 'User 3',
+    lastName: '',
+    statusText: 'In Costa Brava!',
+    ///image: 
+  },
+  {
+    name: 'user 4',
+    lastName: '',
+    statusText: 'In Dubai.',
+   // image:
+  }
+]
+
